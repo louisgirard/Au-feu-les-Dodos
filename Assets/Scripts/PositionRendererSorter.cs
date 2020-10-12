a8d8ba159dabcffac9bd3a9b4a80dfef3aa1aa9d
louisgirard <louis.girard17@hotmail.com>
2020-10-12 16:30:32 +0200

    Added layer order sorter

---
+++ Assets/Scripts/PositionRendererSorter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PositionRendererSorter : MonoBehaviour
+{
+    [SerializeField] float offset = 0f;
+    float timeBetweenCalculation = 0.01f;
+
+    float precision = 100;
+
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (gameObject.isStatic)
+        {
+            UpdateSortingOrder();
+        }
+        else
+        {
+            StartCoroutine(UpdateSortingLayer());
+        }
+    }
+
+    IEnumerator UpdateSortingLayer()
+    {
+        while (true)
+        {
+            UpdateSortingOrder();
+
+            yield return new WaitForSeconds(timeBetweenCalculation);
+        }
+    }
+
+    private void UpdateSortingOrder()
+    {
+        spriteRenderer.sortingOrder = (int)((-transform.position.y + offset) * precision);
+    }
+}
