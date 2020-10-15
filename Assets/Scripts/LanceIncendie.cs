9403359adfb9177f2fd71e3028dc3e98ae30b431
Corentin Laloz <corentin.laloz@hotmail.fr>
2020-10-15 16:56:54 +0200

    Début Lance Incendie

---
+++ Assets/Scripts/LanceIncendie.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanceIncendie : MonoBehaviour
+{
+
+    void Start()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+
+        var main = ps.main;
+        main.startDelay = 0.0f;
+        main.startLifetime = 2.0f;
+        main.startSize = 0.1f;
+
+    }
+
+
+}
