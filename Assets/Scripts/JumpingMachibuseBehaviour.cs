ac4b886f3473db0943da9cb18356ee4ab77fbd69
Antonin-Gaboriau <antonin.GABORIAU@etu.univ-amu.fr>
2020-10-15 06:24:43 +0200

    Basic machibuse (generation from fire, movement and sprite)

---
+++ Assets/Scripts/JumpingMachibuseBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingMachibuseBehaviour : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 player_position;
+    
+    void Update()
+    {
+        Vector3 player_position = GameObject.FindWithTag("Player").transform.position;
+        transform.Translate((player_position - transform.position).normalized * Time.deltaTime * speed);
+    }
+}
