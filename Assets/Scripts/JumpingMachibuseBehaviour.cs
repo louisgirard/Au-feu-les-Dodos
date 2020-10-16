47d93d453d443ee6a1a27690979d3608c50ab1fe
Antonin-Gaboriau <antonin.GABORIAU@etu.univ-amu.fr>
2020-10-16 03:34:59 +0200

    Machibuse jump behaviour

--- Assets/Scripts/JumpingMachibuseBehaviour.cs
+++ Assets/Scripts/JumpingMachibuseBehaviour.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class JumpingMachibuseBehaviour : MonoBehaviour
 {
-    public float speed;
+    public float jump_length;
+    public float jump_duration;
+    public AnimationCurve jump_curve;
+    public float jump_pause;
 
-    private Vector3 player_position;
-    
+    private Vector3 player_position; // final target
+    private Vector3 start_position; // jump start
+    private Vector3 target_position; // jump target
+    private float jump_timer;
+    private float pause_timer;
+
+    private void Start()
+    {
+        jump_timer = jump_duration;
+    }
+
     void Update()
     {
+        // Sprinteur : transform.Translate((player_position - transform.position).normalized * Time.deltaTime * speed);
+
+        jump_timer += Time.deltaTime;
+
+        if (jump_timer >= jump_duration)
+            init_jump();
+
+        else if (jump_timer >= 0)
+            jump();
+    }
+
+    void init_jump()
+    {
+        start_position = transform.position;
         Vector3 player_position = GameObject.FindWithTag("Player").transform.position;
-        transform.Translate((player_position - transform.position).normalized * Time.deltaTime * speed);
+        target_position = Vector3.MoveTowards(start_position, player_position, jump_length);
+        jump_timer = - jump_pause;
+    }
+    
+    void jump()
+    {
+        transform.position = Vector3.Lerp(
+            start_position,
+            target_position,
+            jump_curve.Evaluate(jump_timer / jump_duration));
     }
 }
