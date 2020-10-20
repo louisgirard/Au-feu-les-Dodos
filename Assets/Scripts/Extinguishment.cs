69585a9d3b0cfc8b23442846171d8e00b738ed12
Antonin-Gaboriau <antonin.GABORIAU@etu.univ-amu.fr>
2020-10-20 04:32:04 +0200

    Extinguishment with health time and also for enemies, particles change and minor fixes

---
+++ Assets/Scripts/Extinguishment.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Extinguishment : MonoBehaviour
+{
+    public float health;
+
+    private float timer;
+
+    private void Start()
+    {
+        timer = 0;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Lance"))
+        {
+            if (timer >= health)
+                Destroy(gameObject, 0.1f);
+            else
+                timer += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Lance"))
+        {
+            timer = 0;
+        }
+    }
+}
