e78bb6876f834e53e0fe0438ddc3fa01163e1a03
Corentin Laloz <corentin.laloz@hotmail.fr>
2020-10-23 15:04:05 +0200

    Add EnjoymentBar (start)

---
+++ Assets/Scripts/EnjoymentBar/EnjoymentBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnjoymentBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public Gradient gradient;
+    public Image fill;
+    public void SetMaxEnjoyment(int enjoyment)
+    {
+        slider.maxValue = enjoyment;
+        slider.value = enjoyment;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetEnjoyment(int enjoyment)
+    {
+        slider.value = enjoyment;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
---
+++ Assets/Scripts/EnjoymentBar/PlayerEnjoyment.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerEnjoyment : MonoBehaviour
+{
+    public int maxEnjoyment = 100;
+    public int currentEnjoyment;
+
+    public EnjoymentBar enjoymentBar;
+    
+    void Start()
+    {
+        currentEnjoyment = maxEnjoyment;
+        enjoymentBar.SetMaxEnjoyment(maxEnjoyment);
+    }
+   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+  
+        if (collision.CompareTag("Fire"))
+        {
+            TakePleasure(10);
+        }
+
+        if (collision.CompareTag("Machibuse"))
+        {
+            TakeDamage(2);
+        }
+    }
+    void TakeDamage(int damage)
+    {
+        currentEnjoyment -= damage;
+        enjoymentBar.SetEnjoyment(currentEnjoyment);
+    }
+
+    void TakePleasure(int pleasure)
+    {
+        currentEnjoyment += pleasure;
+        enjoymentBar.SetEnjoyment(currentEnjoyment);
+    }
+}
