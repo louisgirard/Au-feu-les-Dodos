a4f60beaaa50b2032aa464e6269f0a9add1bc37a
Antonin-Gaboriau <antonin.GABORIAU@etu.univ-amu.fr>
2020-10-25 05:16:09 +0100

    Fire rekindle

--- Assets/Scripts/Extinguishment.cs
+++ Assets/Scripts/Extinguishment.cs
@@ -3,6 +3,21 @@
 public class Extinguishment : MonoBehaviour
 {
     public float health;
+    public float rekindle_speed;
+
+    private float max_health;
+
+    private void Start()
+    {
+        max_health = health;
+    }
+
+    private void Update()
+    {
+        if (health < max_health)
+            health += Time.deltaTime * rekindle_speed / 10;
+        print(health);
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
