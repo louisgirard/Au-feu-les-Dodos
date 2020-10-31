28fdf913300ff4b65071d83cd40e2197c0b53f92
louisgirard <louis.girard17@hotmail.com>
2020-10-31 10:43:17 +0100

    Reorder mouse sprites

--- Assets/Scripts/MouseAspect.cs
+++ Assets/Scripts/MouseAspect.cs
@@ -6,8 +6,8 @@ public class MouseAspect : MonoBehaviour
 {
     public Texture2D cursorTexture;
     public Texture2D defaultTexture;
-    public CursorMode cursorMode = CursorMode.Auto;
-    public Vector2 hotSpot = Vector2.zero;
+    private CursorMode cursorMode = CursorMode.Auto;
+    private Vector2 hotSpot = Vector2.zero;
 
     private void Update()
     {
