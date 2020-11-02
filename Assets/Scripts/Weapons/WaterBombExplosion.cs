7b5556c5afe22adeb24d217773169c2b61aa1842
louisgirard <louis.girard17@hotmail.com>
2020-11-02 11:40:08 +0100

    Added time bomb launcher

---
+++ Assets/Scripts/Weapons/WaterBombExplosion.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class WaterBombExplosion : MonoBehaviour
+{
+    [SerializeField] float timeBeforeExplosion = 0f;
+    [SerializeField] ParticleSystem waterExplosionPrefab = null;
+
+    bool isTimerStarted = false;
+    float timer;
+
+    private void Start()
+    {
+        timer = timeBeforeExplosion;
+    }
+
+    void Update()
+    {
+        if(isTimerStarted)
+        {
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                // explosion
+                ParticleSystem waterExplosion = Instantiate(waterExplosionPrefab, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+                Destroy(waterExplosion.gameObject, waterExplosion.main.duration);
+            }
+        }
+    }
+
+    public void StartTimer()
+    {
+        isTimerStarted = true;
+    }
+}
