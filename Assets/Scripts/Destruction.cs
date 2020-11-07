bffc2e2a6534f3564fde80c1830dd69aa31d9023
Corentin Laloz <corentin.laloz@hotmail.fr>
2020-11-07 16:51:56 +0100

    Added Axe

---
+++ Assets/Scripts/Destruction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Destruction : MonoBehaviour
+{
+    public float health;
+
+    public void TakeDamage(float damage)
+    {
+        health = Mathf.Max(health - damage, 0);
+        if (health == 0 && gameObject.CompareTag("Stump"))
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Weapons/Axe.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Axe : Weapon
+{
+    private float weaponRange = 1f;
+    public override void Fire()
+    {
+        int layerMask = 1 << 8;
+
+        Vector2 cursorPosition = CursorPosition.Position();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, cursorPosition, weaponRange, layerMask);
+
+        if (hit.collider != null)
+        {
+            hit.collider.GetComponent<Destruction>().TakeDamage(1);
+        }
+    }
+}
