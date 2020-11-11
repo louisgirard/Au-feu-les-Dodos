e1a0953cad007dd8ab95ea207ac2ae8ccc0df1da
Antonin-Gaboriau <antonin.GABORIAU@etu.univ-amu.fr>
2020-11-11 04:59:45 +0100

    Ectoplasma with two pattern : walk and proximity explosion
    Ectoplasma is just a gameobject in a scene for now, will be a prefab in future

---
+++ Assets/Scripts/Ectoplasma/EctoplasmaExplosion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EctoplasmaExplosion : MonoBehaviour
+{
+    private PlayerEnjoyment playerEnjoyment;
+
+    private void Start()
+    {
+        playerEnjoyment = FindObjectOfType<PlayerEnjoyment>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            playerEnjoyment.TakeDamage("Ectoplasma proximity explosion");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Dodo"))
+            collision.GetComponentInChildren<DodoHealth>().TakeDamage(1);
+    }
+}
---
+++ Assets/Scripts/Ectoplasma/EctoplasmaWalk.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EctoplasmaWalk : StateMachineBehaviour
+{
+    public float speed = 0.7f;
+    private GameObject ectoplasma;
+    private GameObject player;
+    private float proximity_timer;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        ectoplasma = GameObject.FindWithTag("Ectoplasma");
+        player = GameObject.FindWithTag("Player");
+        proximity_timer = 0;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector3 move = (player.transform.position - ectoplasma.transform.position);
+        ectoplasma.transform.Translate(move.normalized * speed * Time.deltaTime);
+
+        if (move.magnitude < 1.5f) // trigger range for proximity explosion
+            proximity_timer += Time.deltaTime;
+        else
+            proximity_timer = 0;
+
+        if (proximity_timer > 1.5f)
+        {
+            animator.SetTrigger("Proximity");
+            proximity_timer = 0;
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+}
