26823c3ce0c8da1980f4cea407f447ccb1b9ce7f
louisgirard <louis.girard17@hotmail.com>
2020-11-26 15:23:48 -0500

    Fix player enjoyment in ectoplasma fight

--- Assets/Scripts/Player/PlayerEnjoyment.cs
+++ Assets/Scripts/Player/PlayerEnjoyment.cs
@@ -31,27 +31,22 @@ public class PlayerEnjoyment : MonoBehaviour
         if (action == "Machibuse")
         {
             LoseEnjoyment(5);
-            enjoymentBar.SetEnjoyment(currentEnjoyment);
         }
         else if (action == "Rodeur")
         {
             LoseEnjoyment(10f);
-            enjoymentBar.SetEnjoyment(currentEnjoyment);
         }
         else if (action == "Burn")
         {
             LoseEnjoyment(0.5f);
-            enjoymentBar.SetEnjoyment(currentEnjoyment);
         }
         else if (timeOn && action == "Time")
         {
             LoseEnjoyment(2);
-            enjoymentBar.SetEnjoyment(currentEnjoyment);
         }
         else if (action == "Ectoplasma fireball")
         {
             LoseEnjoyment(5f);
-            enjoymentBar.SetEnjoyment(currentEnjoyment);
         }
     }
 
@@ -60,33 +55,31 @@ public class PlayerEnjoyment : MonoBehaviour
         if (action == "Fire")
         {
             AddEnjoyment(2.5f);
-            enjoymentBar.SetEnjoyment(currentEnjoyment);
         }
         if (action == "Machibuse Death")
         {
             AddEnjoyment(1f);
-            enjoymentBar.SetEnjoyment(currentEnjoyment);
         }
         if (action == "Rodeur Death")
         {
             AddEnjoyment(13);
-            enjoymentBar.SetEnjoyment(currentEnjoyment);
         }
         if (action == "Rodeur Damage")
         {
             AddEnjoyment(0.015f);
-            enjoymentBar.SetEnjoyment(currentEnjoyment);
         }
     }
 
-    private void AddEnjoyment(float enjoyment)
+    public void AddEnjoyment(float enjoyment)
     {
         currentEnjoyment = Mathf.Min(currentEnjoyment + enjoyment, maxEnjoyment);
+        enjoymentBar.SetEnjoyment(currentEnjoyment);
     }
 
     private void LoseEnjoyment(float enjoyment)
     {
         currentEnjoyment = Mathf.Max(currentEnjoyment - enjoyment, 0);
+        enjoymentBar.SetEnjoyment(currentEnjoyment);
     }
 
     public void ActiveTime(bool val)
