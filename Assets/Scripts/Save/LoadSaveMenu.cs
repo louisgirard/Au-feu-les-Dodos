d68d333535f4fd51298540f1c4de2e86c1f6f1b0
louisgirard <louis.girard17@hotmail.com>
2020-11-28 11:09:39 -0500

    Fix pause menu

---
+++ Assets/Scripts/Save/LoadSaveMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class LoadSaveMenu : MonoBehaviour
+{
+    public void LoadData()
+    {
+        GlobalSaveSystem saveSystem = FindObjectOfType<GlobalSaveSystem>();
+        if(saveSystem)
+            saveSystem.LoadSaveData();
+    }
+}
