e704a47ad4aeb0a790019eb8fb56bcb9a0f07f8a
louisgirard <louis.girard17@hotmail.com>
2020-11-28 10:50:08 -0500

    Added dodo weak dialogue

---
+++ Assets/Scripts/Dialogue/DodoWeakDialogue.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class DodoWeakDialogue : DialogueTrigger
+{
+    DodoHealth dodoHealth;
+    bool dialogueStarted = false;
+
+    private void Start()
+    {
+        dodoHealth = FindObjectOfType<DodoHealth>();
+    }
+
+    private void Update()
+    {
+        if(dodoHealth.IsDying() && !dialogueStarted)
+        {
+            dialogueStarted = true;
+            DialogueStart();
+        }
+    }
+
+    public override void DialogueEnd()
+    {
+        FindObjectOfType<DodoUI>().ResetHead();
+        Destroy(gameObject);
+    }
+}
