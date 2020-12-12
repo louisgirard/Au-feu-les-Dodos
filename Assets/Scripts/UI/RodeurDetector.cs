a42f73761a7bd834e3cb7ff0191ccd443376a5e9
louisgirard <louis.girard17@hotmail.com>
2020-12-12 10:14:30 -0500

    Added dodo in radar

--- Assets/Scripts/UI/RodeurDetector.cs
+++ Assets/Scripts/UI/RodeurDetector.cs
@@ -8,7 +8,11 @@ public class RodeurDetector : MonoBehaviour
     [SerializeField] float pointLimit = 10f;
     [SerializeField] GameObject rodeurPointPrefab = null;
     [SerializeField] GameObject rodeurArrowPrefab = null;
+    [SerializeField] GameObject dodoPoint = null;
+    [SerializeField] GameObject dodoArrow = null;
+
     Transform player;
+    Transform dodo;
     GameObject[] rodeurs;
     List<GameObject> rodeurPoints = new List<GameObject>();
     List<GameObject> rodeurArrows = new List<GameObject>();
@@ -16,13 +20,16 @@ public class RodeurDetector : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        dodo = GameObject.FindGameObjectWithTag("Dodo").transform;
         rodeurs = GameObject.FindGameObjectsWithTag("Rodeur");
         Setup();
     }
 
     void Update()
     {
-        for(int i = 0; i < rodeurs.Length; i++)
+        Vector2 direction;
+        // Rodeurs
+        for (int i = 0; i < rodeurs.Length; i++)
         {
             if(rodeurs[i] == null)
             {
@@ -30,7 +37,7 @@ public class RodeurDetector : MonoBehaviour
                 rodeurPoints[i].SetActive(false);
                 continue;
             }
-            Vector2 direction = (rodeurs[i].transform.position - player.position);
+            direction = (rodeurs[i].transform.position - player.position);
             if(direction.magnitude > arrowLimit)
             {
                 rodeurArrows[i].SetActive(false);
@@ -40,7 +47,7 @@ public class RodeurDetector : MonoBehaviour
             {
                 rodeurPoints[i].SetActive(false);
                 rodeurArrows[i].SetActive(true);
-                OrientateArrow(i, direction);
+                OrientateRodeurArrow(i, direction);
             }
             else
             {
@@ -48,6 +55,26 @@ public class RodeurDetector : MonoBehaviour
                 rodeurPoints[i].SetActive(true);
                 rodeurPoints[i].GetComponent<RectTransform>().anchoredPosition = radar.rect.size / 2 * direction / pointLimit;
             }
+        }
+
+        // Dodo
+        direction = (dodo.position - player.position);
+        if (direction.magnitude > arrowLimit)
+        {
+            dodoArrow.SetActive(false);
+            dodoPoint.SetActive(false);
+        }
+        else if (direction.magnitude > pointLimit)
+        {
+            dodoPoint.SetActive(false);
+            dodoArrow.SetActive(true);
+            OrientateDodoArrow(direction);
+        }
+        else
+        {
+            dodoArrow.SetActive(false);
+            dodoPoint.SetActive(true);
+            dodoPoint.GetComponent<RectTransform>().anchoredPosition = radar.rect.size / 2 * direction / pointLimit;
         }
     }
 
@@ -62,7 +89,18 @@ public class RodeurDetector : MonoBehaviour
         }
     }
 
-    private void OrientateArrow(int index, Vector2 direction)
+    private void OrientateDodoArrow(Vector2 direction)
+    {
+        float angle = Vector2.Angle(new Vector2(0, 1), direction);
+        if (direction.x > 0)
+        {
+            angle = -angle;
+        }
+
+        dodoArrow.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    private void OrientateRodeurArrow(int index, Vector2 direction)
     {
         float angle = Vector2.Angle(new Vector2(0, 1), direction);
         if (direction.x > 0)
