be6a5d9b5c917d4115faa6ba992cac96ef052771
louisgirard <louis.girard17@hotmail.com>
2020-12-18 17:56:17 -0500

    Modified outro, added position render to ectoplasma

--- Assets/Scripts/Outro.cs
+++ Assets/Scripts/Outro.cs
@@ -1,20 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class Outro : MonoBehaviour
 {
     [SerializeField] PlayableDirector outroAnimation = null;
+    [SerializeField] Text outroText = null;
+
+    float fadeTime = 2;
 
     void Start()
     {
         MouseAspect.ChangeAspect(MouseAspect.Aspect.Mouse);
         outroAnimation.stopped += EndAnimation;
+        ChangeAlphaColor(0);
     }
 
     private void EndAnimation(PlayableDirector director)
     {
-        ResetGame();
+        // Fade in text
+        StartCoroutine(TextFadeIn());
+
+        // End Game
+        Invoke("ResetGame", 9f);
     }
 
     private void ResetGame()
@@ -22,5 +32,21 @@ public class Outro : MonoBehaviour
         // End Game
         CrossSceneInformation.Reset();
         SceneManager.LoadScene(0);
+    }
+
+    IEnumerator TextFadeIn()
+    {
+        float totalTime = 0;
+        while(totalTime < fadeTime)
+        {
+            totalTime += fadeTime / 50;
+            ChangeAlphaColor(totalTime / fadeTime);
+            yield return new WaitForSeconds(fadeTime / 50);
+        }
+    }
+
+    private void ChangeAlphaColor(float alpha)
+    {
+        outroText.color = new Color(outroText.color.r, outroText.color.g, outroText.color.b, alpha);
     }
 }
